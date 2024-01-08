3d85ab364efcc6cc8e9ee77c14162ab7d15022c6
Average11 <soheila.naghikhani@gmail.com>
2024-01-08 17:56:32 +0330

    Session3 Average

---
+++ Test3Average.java
@@ -0,0 +1,10 @@
+public class Test3Average {
+    public static void main(String[] args) {
+        int averageVar=Average(3,7);
+        System.out.println(averageVar);
+    }
+    public static int Average(int val1,int val2){
+        int avarage=(val1+val2)/2;
+        return avarage;
+    }
+}
