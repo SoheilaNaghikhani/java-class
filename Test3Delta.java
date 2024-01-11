e205f67d78f4ea3ecae846fd5afa855f0f9e62bd
Average11 <soheila.naghikhani@gmail.com>
2024-01-12 02:12:43 +0330

    Session3

--- Test3Delta.java
+++ Test3Delta.java
@@ -1,5 +1,15 @@
 public class Test3Delta {
     public static void main(String[] args) {
-
+        int x = 1;
+        int equation=3*x*x+6*x+2;
+        int a=3;
+        int b=6;
+        int c=2;
+        int result=Delta(a,b,c);
+        System.out.println(result);
+    }
+    public static int Delta(int a,int b, int c){
+        int deltaAccount=b*b-4*a*c;
+        return deltaAccount;
     }
 }
