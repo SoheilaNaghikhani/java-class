38d76b1964b3e2cd32fafdf1113b247f0b4447cb
Average11 <soheila.naghikhani@gmail.com>
2024-01-18 17:52:04 +0330

    Session3

---
+++ Test3LogicalOperation.java
@@ -0,0 +1,13 @@
+public class Test3LogicalOperation {
+        public static void main(String[] args) {
+            int x=2;
+            int y=3;
+            //OR
+            System.out.println(2==3 || 2<3);
+            System.out.println(2<3 && 3<5);
+            System.out.println(2<3 && 3<1);
+            System.out.println(2 == 5);
+            System.out.println(2 != 5);
+            //XOR
+        }
+    }
